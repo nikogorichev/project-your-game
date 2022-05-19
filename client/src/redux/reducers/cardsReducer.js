eb140fab93048a8a2fd927c363d4bb3c3e07c63e
Anton <anton.yasinyuk@mail.ru>
2022-05-19 21:47:11 +0300

    reqQuestion

--- client/src/redux/reducers/cardsReducer.js
+++ client/src/redux/reducers/cardsReducer.js
@@ -1,3 +1,12 @@
-export const cardsReducer = ({
-  
-})
+import { ADD_CARD } from "../actionTypes/cardsAT"
+const initialState ={cards: []}
+
+export const cardsReducer = (state = initialState, action) => {
+  switch(action.type){
+    case ADD_CARD :
+      return { ...state,cards:action.payload}
+    default: 
+    return state
+  }
+
+}
