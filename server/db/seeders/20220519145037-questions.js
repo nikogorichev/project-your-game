fbc6a42ce78edc7f10868e50e8eda8f2730c8304
Anton <anton.yasinyuk@mail.ru>
2022-05-20 10:39:07 +0300

    вопросики завершил

--- server/db/seeders/20220519145037-questions.js
+++ server/db/seeders/20220519145037-questions.js
@@ -56,7 +56,7 @@ module.exports = {
       answer: 'Анна',
       isActive: true,
       price: 200,
-      id_theme: 1,
+      id_theme: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
@@ -64,7 +64,7 @@ module.exports = {
       answer: 'Дверь',
       isActive: true,
       price: 400,
-      id_theme: 1,
+      id_theme: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     }, {
@@ -72,146 +72,146 @@ module.exports = {
       answer: 'Катя',
       isActive: true,
       price: 600,
-      id_theme: 1,
+      id_theme: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
+    }, {
       question: 'Сколько выпускников повесили на кухне "фотографий" ?',
       answer: '25',
       isActive: true,
       price: 800,
-      id_theme: 1,
+      id_theme: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
+    }, {
       question: 'Рыба или Мясо?',
       answer: 'Птица',
       isActive: true,
       price: 1000,
-      id_theme: 1,
+      id_theme: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
+    }, {
       // тема Elbrus Имена
       question: 'Самое популярное имя в Elbrus?',
       answer: 'Lexa',
       isActive: true,
       price: 200,
-      id_theme: 1,
+      id_theme: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
+    }, {
       question: 'Кто держит марку качества СЭМА ?',
       answer: 'Коля',
       isActive: true,
       price: 400,
-      id_theme: 1,
+      id_theme: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
+    }, {
       question: 'Самая тихая девушка в Elbrus?',
       answer: 'Настя',
       isActive: true,
       price: 600,
-      id_theme: 1,
+      id_theme: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
+    }, {
       question: 'Какое иностраное имя мы используем при регистрации ?',
       answer: 'Игорьбек',
       isActive: true,
       price: 800,
-      id_theme: 1,
+      id_theme: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
+    }, {
       question: 'Кто назвал Алексея преподавателя Бубочкой?',
       answer: 'Анна',
       isActive: true,
       price: 1000,
-      id_theme: 1,
+      id_theme: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
+    }, {
        // тема хиханьки да хахоньки
       question: 'Что Кирил поднял Вове???',
       answer: 'базу данных',
       isActive: true,
       price: 200,
-      id_theme: 1,
+      id_theme: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
-      question: 'Кому на третьей фазе , не третий десяток идет ? ',
-      answer: 'РусРедьюс',
+    }, {
+      question: 'Кому на третьей фазе , четвертый десяток? ',
+      answer: 'Руслан',
       isActive: true,
       price: 400,
-      id_theme: 1,
+      id_theme: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
-      question: '',
-      answer: '',
+    }, {
+      question: 'Куда проще всего пройти кастинг мужчине ?',
+      answer: 'В армию',
       isActive: true,
       price: 600,
-      id_theme: 1,
+      id_theme: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
-      question: '',
-      answer: '',
+    }, {
+      question: 'Где украсть миллион и не сесть ?',
+      answer: 'В тюрьме',
       isActive: true,
       price: 800,
-      id_theme: 1,
+      id_theme: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
-      question: '',
-      answer: '',
+    }, {
+      question: 'Могу забыть написать вопрос, но если это произойдет , введите в ответ Elbrus!',
+      answer: 'Ответ неверный, это мы платим Elbrus за обучение - 1000 ',
       isActive: true,
       price: 1000,
-      id_theme: 1,
+      id_theme: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
-       // тема
-      question: '',
-      answer: '',
+    }, {
+       // тема животные
+      question: 'Прозвище у Дениса из Сов?',
+      answer: 'Ягуар',
       isActive: true,
       price: 200,
-      id_theme: 1,
+      id_theme: 5,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
-      question: '',
-      answer: '',
+    }, {
+      question: 'Кто в этой игре , должен быть в мешке?',
+      answer: 'Кот',
       isActive: true,
       price: 400,
-      id_theme: 1,
+      id_theme: 5,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
-      question: '',
-      answer: '',
+    }, {
+      question: 'Чья морда утыкаеться на йоге в пол?',
+      answer: 'Собаки',
       isActive: true,
       price: 600,
-      id_theme: 1,
+      id_theme: 5,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
-      question: '',
-      answer: '',
+    }, {
+      question: 'Кем становяться студенты на выпускном , под алкоголем ?',
+      answer: 'Оборотнем',
       isActive: true,
       price: 800,
-      id_theme: 1,
+      id_theme: 5,
       createdAt: new Date(),
       updatedAt: new Date(),
-    },{
-      question: '',
-      answer: '',
+    }, {
+      question: 'Как называется один студент группы и его мысли о учебе ?',
+      answer: 'Писец',
       isActive: true,
       price: 1000,
-      id_theme: 1,
+      id_theme: 5,
       createdAt: new Date(),
       updatedAt: new Date(),
     },], {});
